565d704ed5fcd1e35091ab1e21aa5482e7794f9b
koral14 <105457134+koral14@users.noreply.github.com>
2023-10-03 19:56:15 -0700

    installed vite

---
+++ backend/routes/recipesAll.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const { 
+//     getAllRecipes, 
+// } = require('../controllers/recipes');
+
+// router.route('/').get(getAllRecipes);
+
+// module.exports = router;
